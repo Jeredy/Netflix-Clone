23c98db4025a72c6dce0fe1029805627fea13a6b
Andre Rodrigues <andrecomerciale@gmail.com>
2023-06-09 09:04:17 -0300

    feat: created tabBar navigation

---
+++ Netflix/Controllers/DownloadsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadsViewController.swift
+//  Netflix
+//
+//  Created by André Almeida on 2023-06-09.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Netflix/Controllers/UpcomingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingViewController.swift
+//  Netflix
+//
+//  Created by André Almeida on 2023-06-09.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
